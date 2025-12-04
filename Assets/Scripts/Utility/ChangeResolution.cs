c78598f0b37effc7db56c1aad6a4df5283860aa6
JoshG-OTU <joshuageelenfamily@gmail.com>
2025-12-03 19:42:44 -0500

    Added lighting toggle

--- Assets/Scripts/Utility/ChangeResolution.cs
+++ Assets/Scripts/Utility/ChangeResolution.cs
@@ -70,97 +70,13 @@ public class ChangeResolution : MonoBehaviour
                 debug = false;
             }
         }
-    }
-}
-
-public class MaterialSwitcher : MonoBehaviour
-{
-    public Material switchMaterial;
-
-    private readonly Hashtable _matList = new();
-
-    private Renderer[] _renderers;
-
-    private bool switched;
-
-    /// <summary>
-    /// Record all material of the gameobject and its children
-    /// </summary>
-    void Start()
-    {
-        _renderers = GetComponentsInChildren<Renderer>();
-        foreach (Renderer _renderer in _renderers)
+        if (Input.GetButtonDown("Toggle Lighting"))
         {
-            _matList.Add(_renderer, _renderer.material);
+            Light[] allLights = FindObjectsByType<Light>(FindObjectsSortMode.None);
+            foreach (Light light in allLights)
+            {
+                light.enabled = !light.enabled;
+            }
         }
     }
-
-    /// <summary>
-    /// just for testing
-    /// </summary>
-    public void Update()
-    {
-
-        if (Input.GetButtonDown("Toggle Debug"))
-        {
-            ToggleMaterial();
-        }
-    }
-
-    /// <summary>
-    /// Toggle between the switch material and the default.
-    /// </summary>
-    public void ToggleMaterial()
-    {
-        switched = !switched;
-        if (switched)
-        {
-            SwitchMaterial();
-        }
-        else
-        {
-            ResetMaterial();
-        }
-    }
-    /// <summary>
-    /// Revert to the default material
-    /// </summary>
-    public void ResetMaterial()
-    {
-        switched = false;
-        foreach (Renderer _renderer in _renderers)
-        {
-            _renderer.material = _matList[_renderer] as Material;
-        }
-    }
-
-    /// <summary>
-    /// Switch to the predefined switchMaterial
-    /// </summary>
-    public void SwitchMaterial()
-    {
-        switched = true;
-        SetMaterial(switchMaterial); 
-    }
-
-    /// <summary>
-    /// If you want to assign an arbitrary material
-    /// </summary>
-    /// <param name="mat">
-    /// A <see cref="Material"/>
-    /// </param>
-    public void SetMaterial(Material mat)
-    {
-        switched = true;
-        if (mat == null)
-        {
-            Debug.LogWarning("no Material defined to set on GameObject: " + name);
-            return;
-        }
-        foreach (Renderer _renderer in _renderers)
-        {
-            _renderer.material = mat;
-        }
-    }
-
 }
